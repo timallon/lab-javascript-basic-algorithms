338377f8e3780538d14b89e3e539acf8ec491123
Tim Allon <timallon@gmail.com>
2022-10-05 21:39:59 +0100

    done plus bonus question

--- js/index.js
+++ js/index.js
@@ -47,3 +47,36 @@ else if (hacker1.length < hacker2.length) {
 else if (hacker1.length = hacker2.length) {
     console.log("What?! You both have the same name?");
 }
+
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pulvinar vitae ligula et accumsan. Ut suscipit dapibus mollis. Maecenas vitae libero ac nisl rutrum tincidunt ut at est. Maecenas sagittis ex eu venenatis scelerisque. Donec sed vehicula lacus, vitae hendrerit turpis. Mauris sit amet accumsan ante, ac feugiat dui. Integer faucibus lectus ante, quis commodo quam pretium eget. Suspendisse sagittis leo ut lacus vulputate, vitae ullamcorper augue placerat. Duis vitae ultricies augue, ac tempor ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean fermentum, nisi convallis posuere pretium, lectus metus aliquam diam, eget tristique neque urna a mauris. Curabitur mattis neque vitae nisl laoreet, vel sollicitudin nisi elementum. Vivamus interdum erat nunc, egestas commodo felis vehicula in.
+
+Sed ut lectus eget orci tempor accumsan id eget tortor. Curabitur eget tempor orci. Suspendisse vulputate porttitor dui, vitae vehicula risus ornare nec. Praesent consectetur libero vitae imperdiet efficitur. Nulla a ultricies tortor, quis hendrerit dolor. Nulla hendrerit facilisis gravida. Cras non urna enim. Suspendisse rutrum placerat ante, eget porta nulla rutrum eu. Proin urna augue, eleifend eget lacinia eget, molestie non mauris. Nunc bibendum dui magna, cursus ornare tellus accumsan non. Donec sagittis ultrices neque vel faucibus.
+
+Cras at egestas est, quis convallis lectus. Duis eget dapibus lacus, ut facilisis ante. Nulla id convallis purus. Integer vel mollis ligula, ac ultrices mi. Suspendisse sagittis lorem non ipsum accumsan ultricies. Morbi lacus leo, vestibulum ac ligula vitae, ullamcorper consequat turpis. Integer ut libero eget neque finibus vehicula at eget est. Nam rhoncus justo sit amet ex pellentesque, ut porta erat consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed iaculis erat quis pharetra feugiat. Morbi ultricies nibh ac metus scelerisque consectetur. Vivamus aliquet ut libero id sollicitudin. Maecenas et eros quis lectus aliquet lobortis. Mauris pretium lectus quis porttitor sodales. Proin mattis justo in tellus convallis, et molestie nulla placerat. Vivamus pretium massa quis turpis pretium sollicitudin.`;
+
+function subStringCounter (string) {
+    let counter = 0;
+    for ( let i = 0; i <= string.length - 1; i++ ) {
+        const char1 = string[i]; 
+        if ( char1 === " " ) {
+          for ( let i = 0; i <= string.length - 1; i++ ) {
+            const char2 = string[i];
+            if ( char2 === "e" ) {
+                for ( let i = 0; i <= string.length - 1; i++ ) {
+                    const char3 = string[i];
+                    if ( char3 === "t" ) {
+                        for ( let i = 0; i <= string.length - 1; i++ ) {
+                            const char4 = string[i];
+                            if ( char4 === " " ) {
+                                counter += 1;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+          }
+        }
+    }
+}
+console.log(subStringCounter(lorem));
